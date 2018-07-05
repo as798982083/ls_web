c2c7f2c9b1448155aedef92b2ac7bca4306e5b90
as798982083 <798982083@qq.com>
2018-07-05 22:34:06 +0800

    文件上传到服务器的D:/ls_web/upload/文件夹下，通过http://localhost:8080/filename即可访问

--- src/main/java/com/zcj/ls/ls_web/utils/FileUtil.java
+++ src/main/java/com/zcj/ls/ls_web/utils/FileUtil.java
@@ -1,5 +1,7 @@
 package com.zcj.ls.ls_web.utils;
 
+import com.zcj.ls.ls_web.config.WebConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,6 +16,8 @@ import java.util.Calendar;
 
 public class FileUtil {
 
+    @Autowired
+    public static WebConfig webConfig;
     /**
      * 保存文件，并返回文件的相对路径。
      * @param file 临时文件名称
@@ -30,8 +34,12 @@ public class FileUtil {
         //MultipartFile的方法直接写文件
         try {
             //保存文件的绝对路径
-            String path = ResourceUtils.getURL("src\\main\\resources\\static\\upload").getPath();
-            File folder = new File(path.substring(1, path.length()));
+            //获取服务器的绝对路径
+            String path = "D:\\ls_web\\upload";
+            File folder = new File(path);
+            //获取项目路径
+//            String path = ResourceUtils.getURL("src\\main\\resources\\static\\upload").getPath();
+//            File folder = new File(path.substring(1, path.length()));
             //创建目录和文件
             if(!folder.exists()){
                 folder.mkdirs();
@@ -39,7 +47,7 @@ public class FileUtil {
             File file1 = new File(folder.getPath()+"\\"+fileName);
             //写到文件中并保存相对路径
             file.transferTo(file1);
-            url = "upload/"+fileName;
+            url = fileName;
 
         } catch (IllegalStateException | IOException e) {
             e.printStackTrace();
